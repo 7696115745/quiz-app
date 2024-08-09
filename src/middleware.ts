df8f2579674cb8b18dd351e425de7947d471c36f
rahul kumar <rs009161@gmail.com>
2024-08-09 15:35:24 +0530

    work

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,31 +1,28 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse,NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
-  // Allow requests for static assets to pass through
   if (pathname.startsWith('/_next/') || pathname.startsWith('/static/')) {
-    return NextResponse.next();
+      return NextResponse.next();
   }
 
-  // Allow all requests to /api/auth/* to pass through
   if (pathname.startsWith('/api/auth/')) {
-    return NextResponse.next();
+      return NextResponse.next();
   }
 
-  // Skip middleware for the login page
   if (pathname === '/login') {
-    return NextResponse.next();
+      return NextResponse.next();
   }
 
   const adminToken = req.cookies.get("email");
   const userToken = req.cookies.get("authjs.session-token");
 
-  // Redirect to login if neither token is found
   if (!adminToken && !userToken) {
-    return NextResponse.redirect(new URL("/login", req.url));
+      console.log("No tokens found. Redirecting to login.");
+      return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  // Allow access to all routes if any token is present
-  return NextResponse.next();
+  console.log("Token found. Proceeding to next.");
+  return NextResponse.next();   
 }
