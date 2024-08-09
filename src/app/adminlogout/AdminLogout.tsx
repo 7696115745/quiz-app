71923a46c5673babf518c5bcb931452877d58d42
rahul kumar <rs009161@gmail.com>
2024-08-09 11:15:09 +0530

    change in admin logout

--- src/app/adminlogout/AdminLogout.tsx
+++ src/app/adminlogout/AdminLogout.tsx
@@ -1,17 +1,30 @@
 "use client";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
-export default function page() {
-  const Route = useRouter();
-  const HandleAdminLogOut = () => {
+
+export default function Page() {
+  const router = useRouter();
+
+  const handleAdminLogOut = () => {
     Cookies.remove("email");
-  Route.push("/login");
+    // Use `router.push` to navigate to the login page
+    router.push("/login");
   };
+
+  // Redirect on component mount if necessary
+  useEffect(() => {
+    // Check if a certain condition is met (e.g., user is not logged in)
+    if (!Cookies.get("email")) {
+      router.push("/login");
+    }
+  }, [router]);
+
   return (
     <>
       <button
-        onClick={HandleAdminLogOut}
-        className="text-white  font-semibold bg-blue-500 px-2 py-2 rounded-xl absolute top-[30%] left-1/2 translate-x-[-50%] translate-y-[-30%]"
+        onClick={handleAdminLogOut}
+        className="text-white font-semibold bg-blue-500 px-2 py-2 rounded-xl absolute top-[30%] left-1/2 translate-x-[-50%] translate-y-[-30%]"
       >
         Logout
       </button>
